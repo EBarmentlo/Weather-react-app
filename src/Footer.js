1053de040f238bc3c641027df360146ac9b5e55a
EBarmentlo <93001558+EBarmentlo@users.noreply.github.com>
2022-05-13 11:33:39 +0200

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <h5>
+      This app was coded by Elisabeth and is {}
+      <a href="https://github.com/EBarmentlo/weather-react" target="_blank">
+        open-source
+      </a>
+    </h5>
+  );
+}
